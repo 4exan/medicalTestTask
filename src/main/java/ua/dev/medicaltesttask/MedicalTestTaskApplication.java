799b4d7bee065921c841360fed5037f6ce00f48b
4exan <ivan.chekhanovskyi@gmail.com>
2025-02-16 18:31:48 +0200

    Init commit

---
+++ src/main/java/ua/dev/medicaltesttask/MedicalTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ua.dev.medicaltesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/dev/medicaltesttask/MedicalTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dev.medicaltesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
